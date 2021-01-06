bcf4ac3971c6f2e75a33365624782b8f14ce49c8
tool <tool@DESKTOP-590T83K>
2021-01-05 23:12:45 -0300

    Muestra

--- TiendaV1/src/main/java/com/Tienda/Controller/WebController.java
+++ TiendaV1/src/main/java/com/Tienda/Controller/WebController.java
@@ -2,6 +2,8 @@ package com.Tienda.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.Tienda.service.PokedexImpl;
 import com.Tienda.service.PokemonImpl;
@@ -10,14 +12,16 @@ import com.Tienda.service.UsuarioImpl;
 @Controller
 public class WebController {
 
-	@Autowired
-	PokedexImpl pokedexServ;	
-	@Autowired
-	PokemonImpl pokemonServ;
-	@Autowired
-	UsuarioImpl usuarioServ;
+	@Autowired PokedexImpl pokedexServ;	
+	@Autowired PokemonImpl pokemonServ;
+	@Autowired UsuarioImpl usuarioServ;
 	
-	
+	@GetMapping("/")
+	public void getHome (Model model) {
+		pokedexServ.listarPokedexs();
+		pokemonServ.borrarUnPokemon(0);
+		usuarioServ.buscarUsuarioPorId(0);
+	}
 	
 	
 }
